6e779d1b66311cc8917a2d048c04849fa0627729
Yogesh-chouhan-hub <yogeshchouhan2253@gmail.com>
2026-05-25 17:31:55 +0530

    Fixed Display bugs

--- init/index.js
+++ init/index.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const listing = require("../models/listing.js");
+const path = require("path");
+
+require("dotenv").config({
+  path: path.resolve(__dirname, "../.env"),
+});
 
 main()
   .then(() => {
@@ -11,14 +16,14 @@ main()
   });
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/wanderlust");
+  await mongoose.connect(process.env.MONGO_URL);
 }
 
 const initDB = async () => {
   await listing.deleteMany({});
   initData.data = initData.data.map((el) => ({
     ...el,
-    owner: "6a0beb3e6b1ae8792e871351",
+    owner: "6a1436ad23c69e51902bddc2",
   }));
   await listing.insertMany(initData.data);
   console.log("data was initialised");
